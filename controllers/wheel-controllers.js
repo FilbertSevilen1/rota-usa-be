500c974b709efb949d28204e8c0686cf16ad0642
Ignatius Filbert Sevilen <sevilenfilbert@gmail.com>
2024-04-06 15:20:00 +0700

    Commit

--- controllers/wheel-controllers.js
+++ controllers/wheel-controllers.js
@@ -15,32 +15,32 @@ module.exports.getAllWheels = async(req,res) =>{
             const GET_WHEELS_DETAILS = "SELECT * FROM wheels_details WHERE wheel_id = ? && is_active = 1"
             const [DETAILS] = await database.execute(GET_WHEELS_DETAILS, [WHEELS[i].wheel_id]);
 
-            const GET_SIZES = "SELECT * FROM sizes WHERE wheel_id = ?"
-            const [SIZES] = await database.execute(GET_SIZES, [WHEELS[i].wheel_id]);
+            // const GET_SIZES = "SELECT * FROM sizes WHERE wheel_id = ?"
+            // const [SIZES] = await database.execute(GET_SIZES, [WHEELS[i].wheel_id]);
 
-            for(let j=0;j<SIZES.length;j++){
-                const GET_OFFSET = "SELECT * FROM offsets WHERE size_id = ?"
-                const [OFFSET] = await database.execute(GET_OFFSET, [SIZES[j].size_id]);
+            // for(let j=0;j<SIZES.length;j++){
+            //     const GET_OFFSET = "SELECT * FROM offsets WHERE size_id = ?"
+            //     const [OFFSET] = await database.execute(GET_OFFSET, [SIZES[j].size_id]);
 
-                for(let k=0;k<OFFSET.length;k++){
-                    const GET_BOLT = "SELECT * FROM bolts WHERE offset_id = ?"
-                    const [BOLT] = await database.execute(GET_BOLT, [OFFSET[k].offset_id]);
-                    OFFSET[k] = {
-                        ...OFFSET[k],
-                        bolt_details:BOLT
-                    }
-                }
+            //     for(let k=0;k<OFFSET.length;k++){
+            //         const GET_BOLT = "SELECT * FROM bolts WHERE offset_id = ?"
+            //         const [BOLT] = await database.execute(GET_BOLT, [OFFSET[k].offset_id]);
+            //         OFFSET[k] = {
+            //             ...OFFSET[k],
+            //             bolt_details:BOLT
+            //         }
+            //     }
 
-                SIZES[j] = {
-                    ...SIZES[j],
-                    offset_details:OFFSET
-                }
-            }
+            //     SIZES[j] = {
+            //         ...SIZES[j],
+            //         offset_details:OFFSET
+            //     }
+            // }
 
             WHEELS[i] = {
-                ...WHEELS[i],
+                // ...WHEELS[i],
                 wheel_details:DETAILS,
-                size_details:SIZES,
+                // size_details:SIZES,
             }
         }
         res.status(200).send(WHEELS)
